14a104e43f825f386b29f1f8ffd183eaea98e4ac
Vlad <vv@lineup.com.ua>
2020-07-20 15:34:12 +0300

    del old street name

--- backend/views/house/find-result.php
+++ backend/views/house/find-result.php
@@ -82,16 +82,16 @@ $statuses = [
             },
             'contentOptions' => ['style' => 'min-width: 150px; overflow: hidden' ],
         ],
-        [
-            'format' => 'html',
-            'attribute' => 'street_id',
-            'value' =>  function ($dataProvider) {
-                $street = Street::findOne($dataProvider->street_id)->name;
-                $str = str_replace(' ', ' <br>', $street);
-                return /*$str*/$street;
-            },
-            'contentOptions' => ['style' => 'max-width: 70px; overflow: hidden' ],
-        ],
+//        [
+//            'format' => 'html',
+//            'attribute' => 'street_id',
+//            'value' =>  function ($dataProvider) {
+//                $street = Street::findOne($dataProvider->street_id)->name;
+//                $str = str_replace(' ', ' <br>', $street);
+//                return /*$str*/$street;
+//            },
+//            'contentOptions' => ['style' => 'max-width: 70px; overflow: hidden' ],
+//        ],
         'number_building',
         'count_room',
         [
