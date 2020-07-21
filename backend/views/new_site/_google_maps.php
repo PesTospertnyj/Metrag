430597037462052ee3de145ccd86a15fc71c4a72
Vlad <vv@lineup.com.ua>
2020-07-21 16:11:10 +0300

    fix google maps lang

--- backend/views/new_site/_google_maps.php
+++ backend/views/new_site/_google_maps.php
@@ -1,5 +1,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=<?= Yii::$app->params['googleMapsKey']; ?>&libraries=places&callback=initMap1&language=ru" defer></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=<?= Yii::$app->params['googleMapsKey'];
+?>&libraries=places&callback=initMap1&language=ru&region=UA" defer></script>
 <script>
     /*************************Autocomplite*****************************/
     let map;
